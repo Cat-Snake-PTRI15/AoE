aa0e465c912dad8088a03a3624f640b584cc9441
Lia Hwang <ch646063@gmail.com>
2024-07-29 21:59:57 -0700

    after merge some

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ import NotFound from './pages/NotFound.jsx';
 import LogIn from './pages/LogIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Chat from './pages/Chat.jsx';
+import Messages from './pages/Messages.jsx';
 import store from './redux/store';
 import SupabaseLogin from './pages/SupabaseLogin.jsx';
 import Messages from './pages/Messages.jsx';
 import Message1 from './pages/Message1.jsx'
 import './App.scss';
-
 
 const App = () => {
   return (
@@ -28,7 +28,7 @@ const App = () => {
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<LogIn />} />
             <Route path="/profile/*" element={<Profile />} />
-            <Route path="/chat" element={<Chat />} />
+            {/* <Route path="/chat" element={<Chat />} /> */}
             <Route path="/supabaselogin" element={<SupabaseLogin />} />
             <Route path="/messages" element={<Message1 />} />
             <Route path="*" element={<NotFound />} />
@@ -38,6 +38,5 @@ const App = () => {
     </Provider>
   );
 };
-
 
 export default App;
